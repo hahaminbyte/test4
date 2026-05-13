25d4451ecfca22a2789a1f2e138e56129ad67461
Gabriel Fukushima <308015799+hahaminbyte@users.noreply.github.com>
2026-05-13 21:37:44 +0000

    Initial project setup with Vite and TypeScript configuration

---
+++ client/vite.config.ts
@@ -0,0 +1,74 @@
+import * as path from 'path';
+/// <reference types="vitest" />
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { dependencies } from './package.json';
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router', 'react-dom'].includes(key)) return;
+    // @ts-expect-error - error with vite-plugin-eslint
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  build: {
+    sourcemap: true,
+    outDir: 'build',
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+  plugins: [viteReact(), viteTsconfigPaths()],
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    sequence: {
+      hooks: 'parallel',
+    },
+    coverage: {
+      provider: 'v8', // or 'istanbul'
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        '**/node_modules/**',
+        '**/build/**',
+        '**/dist/**',
+        '**/coverage/**',
+        '**/app/setupTests.ts',
+        '**/app/reportWebVitals.ts',
+        '**/public/**',
+        '**/*.d.ts',
+        '**/index.ts',
+        '**/*.config.*',
+        '**/components/Charts/ManifestStatusPieChart/ManifestStatusPieChart.tsx',
+      ],
+    },
+    globals: true,
+    setupFiles: ['./app/mocks/setupTests.ts'],
+  },
+});
