2b4f953f6cf55e7109f6bb7f6510ccde63113b9b
Gabriel Fukushima <308015799+hahaminbyte@users.noreply.github.com>
2026-05-21 21:33:44 +0000

    Added routing for main application features

---
+++ client/app/routes.tsx
@@ -0,0 +1,96 @@
+import { createBrowserRouter } from 'react-router';
+import { ErrorPage } from '~/features/errorPage/ErrorPage';
+import { Login } from '~/features/login';
+import { Component as PrivateRoute } from '~/features/privateRoute';
+import { Register } from '~/features/register';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PrivateRoute />,
+    children: [
+      {
+        path: '',
+        errorElement: <ErrorPage code={500} />,
+        lazy: () => import('~/features/layout'),
+        children: [
+          {
+            path: 'organization',
+            lazy: () => import('~/features/org'),
+          },
+          {
+            path: '',
+            lazy: () => import('~/features/dashboard'),
+          },
+          {
+            path: 'profile',
+            lazy: () => import('~/features/profile'),
+          },
+          {
+            path: 'site',
+            children: [
+              {
+                path: '',
+                lazy: () => import('~/features/siteList'),
+              },
+              {
+                path: ':siteId',
+                lazy: () => import('~/features/siteDetails'),
+              },
+              {
+                path: ':siteId/manifest',
+                children: [
+                  {
+                    path: '',
+                    lazy: () => import('~/features/manifestList'),
+                  },
+                  {
+                    path: 'new',
+                    lazy: () => import('~/features/newManifest'),
+                  },
+                  {
+                    path: ':mtn/:action',
+                    lazy: () => import('~/features/manifestDetails'),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: 'manifest',
+            children: [
+              {
+                path: '',
+                lazy: () => import('~/features/manifestList'),
+              },
+              {
+                path: 'new',
+                lazy: () => import('~/features/newManifest'),
+              },
+              {
+                path: ':mtn/:action',
+                lazy: () => import('~/features/manifestDetails'),
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: 'register',
+    element: <Register />,
+  },
+  {
+    path: 'about',
+    lazy: () => import('~/features/about'),
+  },
+  {
+    path: '*',
+    element: <ErrorPage code={404} />,
+  },
+]);
