ffdc94d0f634f1093bf912e5394332d2196b5273
Abhivansh <308015799+hahaminbyte@users.noreply.github.com>
2026-08-05 23:09:44 +0000

    test(Search): add tests for `client/app/components/Manifest/Handler/Search/HandlerSearchModal.tsx`

---
+++ client/app/components/Manifest/Handler/Search/HandlerSearchModal.tsx
@@ -0,0 +1,71 @@
+import { HtModal } from '~/components/legacyUi';
+
+import { Col, Row } from 'react-bootstrap';
+import { useFieldArray, useFormContext } from 'react-hook-form';
+import { HandlerSearchForm } from '~/components/Manifest/Handler';
+import { Manifest, Transporter } from '~/components/Manifest/manifestSchema';
+import { useHandlerSearchConfig } from '~/hooks/manifest/useOpenHandlerSearch/useHandlerSearchConfig';
+
+/**
+ * Returns a modal that wraps around the HandlerSearchForm for adding the manifest TSDF
+ * @constructor
+ */
+export function HandlerSearchModal() {
+  const manifestForm = useFormContext<Manifest>();
+  const [configs, setConfigs] = useHandlerSearchConfig();
+
+  const transporterForm = useFieldArray<Manifest, 'transporters'>({
+    control: manifestForm.control,
+    name: 'transporters',
+  });
+  const transporters: Transporter[] = manifestForm.getValues('transporters');
+
+  if (!configs) return null;
+  const { siteType, open } = configs;
+
+  const handleClose = () => {
+    setConfigs(undefined);
+  };
+
+  // set the title and description based on the handlerType
+  let title;
+  let description;
+  if (siteType === 'designatedFacility') {
+    title = 'Add Designated Facility';
+    description =
+      'Search by EPA ID. Without RCRAInfo credentials, try local demo ID MOCKTSDFS001.';
+  } else if (siteType === 'transporter') {
+    title = 'Add Transporter';
+    description =
+      'Search by EPA ID. Without RCRAInfo credentials, try local demo IDs MOCKTRANS001 or MOCKTRANS002.';
+  } else {
+    title = 'Add Generator';
+    description =
+      'The site that generated the hazardous waste to be shipped for offsite treatment.';
+  }
+
+  return (
+    <HtModal showModal={open ? open : false} handleClose={handleClose}>
+      <HtModal.Header closeButton>
+        <Col>
+          <Row>
+            <HtModal.Title title={title} />
+          </Row>
+          <Row>
+            <i>
+              <small>{description}</small>
+            </i>
+          </Row>
+        </Col>
+      </HtModal.Header>
+      <HtModal.Body>
+        <HandlerSearchForm
+          handleClose={handleClose}
+          handlerType={siteType}
+          currentTransporters={transporters}
+          appendTransporter={transporterForm.append}
+        />
+      </HtModal.Body>
+    </HtModal>
+  );
+}
